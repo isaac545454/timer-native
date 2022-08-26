5c5044e39de0fb0f154cee5b4520c515d169d703
isaac gomes <93602614+isaac545454@users.noreply.github.com>
2022-08-26 19:27:53 -0300

    fim

--- App.js
+++ App.js
@@ -1,78 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
-import { useState } from 'react'
+import { useState, Component } from 'react'
 
-export default function App() {
-  const [frase, setFrase] = useState("")
-  const [imgB, setImgB] = useState(require('./src/biscoito.png'))
 
-  const quebrarBiscoito = ()=>{
-    const numeroAleatorio = Math.floor(Math.random() * frases.length)
-    setFrase(`" ${frases[numeroAleatorio]} "`)
-    setImgB(require('./src/biscoitoAberto.png'))
+class App extends Component{
+  constructor(props) {
+    super(props)
+    this.state={
+      numero: 0,
+      botao: "vai",
+      ultimo: null
+    }
+    this.timer = null
+    this.iniciar = this.iniciar.bind(this)
+    this.limpar = this.limpar.bind(this)
   }
 
-   const frases = [
-    'O que sabemos é uma gota; o que ignoramos é um oceano.',
-    'Se eu vi mais longe, foi por estar sobre ombros de gigantes.',
-    'Construímos muros demais e pontes de menos.',
-    'Deve-se aprender sempre, até mesmo com um inimigo.',
-    'Virtude sem caridade não passa de nome.',
-
-   ]
+  iniciar(){
+    if (this.timer != null) {
+      clearInterval(this.timer)
+      this.timer = null
+      this.setState({botao: 'vai'})
+    }else{
+    this.timer = setInterval(() => {
+      this.setState({numero: this.state.numero + 0.1})
+    }, 100);
+    this.setState({botao: "PARAR"})
+    }
+  }
+  limpar(){
+    if(this.timer != null){
+      clearInterval(this.timer)
+      this.timer = null
+    }
+    this.setState({numero: 0, botao: 'vai'})
+  }
+   
+render(){
   return (
     <View style={styles.container}>
-      <Image 
-      source={imgB}
-      style={styles.img}
-      />
-      
-      <Text style={styles.textFrase}>{frase}</Text>
-      <TouchableOpacity style={styles.botao} onPress={quebrarBiscoito}>
-        <View style={styles.btnArea}>
-          <Text style={styles.btnText}>quebrar biscoito</Text>
-        </View>
-      </TouchableOpacity>
-      
+    <Image 
+    source={require("./src/cronometro.png")}
+    style={styles.cronometro}
+    />
+    <Text style={styles.timer}>{this.state.numero.toFixed(1)}</Text>
+
+      <View style={styles.areaBtn}>
+
+        <TouchableOpacity style={styles.btn} onPress={this.iniciar}>
+          <Text style={styles.textBtn}>{this.state.botao}</Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+          <Text style={styles.textBtn}>limpar</Text>
+          </TouchableOpacity>
+
+      </View>
     </View>
   );
 }
+}
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#111111',
+    backgroundColor: '#00AEEF',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: -30
   },
-  img: {
-      width: 250,
-      height: 250
-  },
-  textFrase: {
-    fontSize: 20,
-    color: '#dd7b22',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center'
-  },
-  botao: {
-   width: 230,
-   height: 50,
-   borderWidth: 2, 
-   borderColor:  '#dd7b22',
-   borderRadius: 25,
-  },
-  btnArea: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  btnText: {
+  timer: {
+    marginTop: -160,
+    color: '#fff',
+    fontSize: 65,
     fontWeight: 'bold',
-    fontSize: 18,
-    color: '#dd7b22'
-  }
+  },
+  areaBtn: {
+    flexDirection: 'row',
+    marginTop: 90,
+    height: 40
+  },
+  btn: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    height: 40,
+    margin: 20,
+    borderRadius: 10,
+  },
+  textBtn: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: "#00aeef",
 
+  }
 });
